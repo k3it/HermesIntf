ecdd80426a1f3826a9cdd0d447d2d2f46284dcc6
n1gp <n1gp@hotmail.com>
2021-07-04 15:51:07 -0400

    Protocol2 changes, initial support includes Alex in bypass, ADC0 only.

--- HermesIntf/Hermes.h
+++ HermesIntf/Hermes.h
@@ -25,6 +25,26 @@
 #pragma once
 
 #define MAX_RX_COUNT  8
+#define LONG_MSG  1444
+#define SHORT_MSG 60
+
+// port definitions from host
+#define GENERAL_REGISTERS_FROM_HOST_PORT 1024
+#define RECEIVER_SPECIFIC_REGISTERS_FROM_HOST_PORT 1025
+#define HIGH_PRIORITY_FROM_HOST_PORT 1027
+
+// port definitions to host
+#define COMMAND_RESPONSE_TO_HOST_PORT 1024
+#define HIGH_PRIORITY_TO_HOST_PORT 1025
+#define MICROPHONE_DATA_TO_HOST 1026
+#define RX_IQ_TO_HOST_PORT_0 1035
+#define RX_IQ_TO_HOST_PORT_1 1036
+#define RX_IQ_TO_HOST_PORT_2 1037
+#define RX_IQ_TO_HOST_PORT_3 1038
+#define RX_IQ_TO_HOST_PORT_4 1039
+#define RX_IQ_TO_HOST_PORT_5 1040
+#define RX_IQ_TO_HOST_PORT_6 1041
+#define RX_IQ_TO_HOST_PORT_7 1042
 
 namespace HermesIntf
 {
@@ -39,13 +59,18 @@ namespace HermesIntf
 			char mac[25];
 			char *status;
 			int  ver;
+			int  prot_ver;
 			int  clock;
 			int  sample_rates[3];
 			char *ip_addr;
 			int max_recvrs;
 			int rxCount;
+			char recvMSG[LONG_MSG];
 			SOCKET sock;
 			struct sockaddr_in Hermes_addr;
+			struct sockaddr_in Recv_addr;
+			struct sockaddr_in HP_addr;
+			struct sockaddr_in Data_addr[MAX_RX_COUNT];
 
 			// keep track of the selected parameters
 			int selected_LO_freqs[MAX_RX_COUNT];
@@ -61,11 +86,16 @@ namespace HermesIntf
 		~Hermes(void);
 		int StopCapture(void);
 		int StartCapture(int RxCount, int SampleRate);
+		int StopCapture2(void);
+		int StartCapture2(int RxCount, int SampleRate);
 		int Discover(void);
+		void Ping(void);
 		int SetLO(int RecvrID, int Frequency);
+		int SetLO2(int RecvrID, int Frequency);
 
 		//Attenuator control functions
 		void SetAtt(int AttDb);
+		void SetAtt2(int AttDb);
 		void SetMaxAtt(void);
 		void IncrAtt(void);
 		void DecrAtt(void);
